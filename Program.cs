92777f4636ea521c459bb1c81222bc9eb29810ce
MissShreya <shreyanshisrivastava75@gmail.com>
2024-12-23 21:17:16 +0530

    Call objects of default and parameterized constructor.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System;
 class Program{
     public static void Main(string[] args){
+     // Normal Class & Object
+     Console.WriteLine("\n\n Normal Class & Object Code: ");
         Application StudentData1= new Application{Name = "Shreyanshi Srivastava",Rollnumber = 1,Grade ="A"};
         Application StudentData2= new Application{Name ="Rahul Gupta",Rollnumber = 2,Grade="B"};
         Console.WriteLine("Students Detail:-");
@@ -9,11 +11,19 @@ class Program{
         StudentData2.PrintStudentDetail();
 
 
-Constructor defaultconstructor1=new Constructor{Name="Shreya Srivastava" ,Grade="B"};
-Constructor defaultconstructor2=new Constructor{Name="Priya Singh" };
-Constructor defaultconstructor3=new Constructor{Name="Jagreeti Singh" ,Grade="C"};
+//Default Constructor
+Console.WriteLine("\n\n Default Constructor Code: ");
+Student defaultconstructor1=new Student{Name="Shreya Srivastava" ,Grade="B"};
+Student defaultconstructor2=new Student{Name="Priya Singh" };
+Student defaultconstructor3=new Student{Name="Jagreeti Singh" ,Grade="C"};
 defaultconstructor1.PrintDetail();
 defaultconstructor2.PrintDetail();
 defaultconstructor3.PrintDetail();
+
+
+//Parameterized Constructor
+Console.WriteLine("\n\n Parameterized Constructor Code: ");
+Student parameterizedConstructor = new Student("Shreyanshi Srivastava", 1, "A");
+parameterizedConstructor.PrintDetail();
     }
 } 
