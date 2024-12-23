dbbd446cc02397096f65c92d987fcd408f1b2099
MissShreya <shreyanshisrivastava75@gmail.com>
2024-12-23 21:16:17 +0530

    Default and Parameterized Constructor code add.

--- Constructor.cs
+++ Constructor.cs
@@ -1,12 +1,17 @@
-public class Constructor{
-    public required string Name {get;set;}
+public class Student{
+    public string? Name {get;set;}
     public  int ? Roll {get;set;}
     public string ? Grade {get;set;}
 
-    public Constructor(){
+    public Student(){
         Name="Unknown";
         Roll=0;
         Grade="Not Declare";
+    }
+    public Student(string name,int roll,string grade){
+        Name=name;
+        Roll =roll;
+        Grade=grade;
     }
 
     public void PrintDetail(){
